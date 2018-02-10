c481d3c9ee2a40ecbd27511f13b72bb5eb3aadd1
Elena Krasnikova <elena.krasnikova.94@mail.ru>
2018-02-10 21:19:31 +0300

    module 5-task1

---
+++ js/synchronize-fields.js
@@ -0,0 +1,15 @@
+'use strict';
+(function () {
+  var syncValues = function (element, value) {
+    element.value = value;
+  };
+  var synchronizeFields = function (firstField, secondField, firstValues, secondValues, callback) {
+
+  };
+  window.synchronizeFields(checkinTime, checkoutTime, ['12', '13', '14'], ['12', '13', '14'], syncValues);
+  var syncValueWithMin = function (element, value) {
+    element.min = value;
+  };
+
+
+})();
